9cd6342023eda8460a7b6872edab1ffa2ae5b973
alec-muller <alec.r.muller@gmail.com>
2022-12-29 17:52:55 -0300

    compose airflow funcional

--- airflow/dags/example_taskflow_api.py
+++ airflow/dags/example_taskflow_api.py
@@ -6,9 +6,9 @@ from airflow.utils.dates import days_ago
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'airflow',
+    'owner': 'alec',
 }
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['example'])
+@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['alec'])
 def tutorial_taskflow_api_etl():
     """
     ### TaskFlow API Tutorial Documentation
